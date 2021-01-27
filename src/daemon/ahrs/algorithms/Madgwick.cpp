12e9c69502128d651639962900057760703cc8bc
philmccarthy <phil_mccarthy24@hotmail.com>
2021-01-27 09:58:51 +0000

    Madgwick code fixed

--- src/daemon/ahrs/algorithms/Madgwick.cpp
+++ src/daemon/ahrs/algorithms/Madgwick.cpp
@@ -55,6 +55,9 @@ namespace sbrcontroller {
 					return;
 				}
 
+				Axis3DSensorData acc = accelData;
+				Axis3DSensorData mag = magData;
+
 				// Rate of change of quaternion from gyroscope
 				qDot1 = 0.5f * (-m_q.x * gyroData.x - m_q.y * gyroData.y - m_q.z * gyroData.z);
 				qDot2 = 0.5f * (m_q.w * gyroData.x + m_q.y * gyroData.z - m_q.z * gyroData.y);
@@ -62,25 +65,25 @@ namespace sbrcontroller {
 				qDot4 = 0.5f * (m_q.w * gyroData.z + m_q.x * gyroData.y - m_q.y * gyroData.x);
 
 				// Compute feedback only if accelerometer measurement valid (avoids NaN in accelerometer normalisation)
-				if(!((accelData.x == 0.0f) && (accelData.y == 0.0f) && (accelData.z == 0.0f))) {
+				if(!((acc.x == 0.0f) && (acc.y == 0.0f) && (acc.z == 0.0f))) {
 
 					// Normalise accelerometer measurement
-					recipNorm = invSqrt(accelData.x * accelData.x + accelData.y * accelData.y + accelData.z * accelData.z);
-					accelData.x *= recipNorm;
-					accelData.y *= recipNorm;
-					accelData.z *= recipNorm;   
+					recipNorm = invSqrt(acc.x * acc.x + acc.y * acc.y + acc.z * acc.z);
+					acc.x *= recipNorm;
+					acc.y *= recipNorm;
+					acc.z *= recipNorm;   
 
 					// Normalise magnetometer measurement
 					recipNorm = invSqrt(magData.x * magData.x + magData.y * magData.y + magData.z * magData.z);
-					mx *= recipNorm;
-					my *= recipNorm;
-					mz *= recipNorm;
+					mag.x *= recipNorm;
+					mag.y *= recipNorm;
+					mag.z *= recipNorm;
 
 					// Auxiliary variables to avoid repeated arithmetic
-					_2q0mx = 2.0f * m_q.w * mx;
-					_2q0my = 2.0f * m_q.w * my;
-					_2q0mz = 2.0f * m_q.w * mz;
-					_2q1mx = 2.0f * m_q.x * mx;
+					_2q0mx = 2.0f * m_q.w * mag.x;
+					_2q0my = 2.0f * m_q.w * mag.y;
+					_2q0mz = 2.0f * m_q.w * mag.z;
+					_2q1mx = 2.0f * m_q.x * mag.x;
 					_2q0 = 2.0f * m_q.w;
 					_2q1 = 2.0f * m_q.x;
 					_2q2 = 2.0f * m_q.y;
@@ -99,18 +102,18 @@ namespace sbrcontroller {
 					q3q3 = m_q.z * m_q.z;
 
 					// Reference direction of Earth's magnetic field
-					hx = mx * q0q0 - _2q0my * m_q.z + _2q0mz * m_q.y + mx * q1q1 + _2q1 * my * m_q.y + _2q1 * mz * m_q.z - mx * q2q2 - mx * q3q3;
-					hy = _2q0mx * m_q.z + my * q0q0 - _2q0mz * m_q.x + _2q1mx * m_q.y - my * q1q1 + my * q2q2 + _2q2 * mz * m_q.z - my * q3q3;
+					hx = mag.x * q0q0 - _2q0my * m_q.z + _2q0mz * m_q.y + mag.x * q1q1 + _2q1 * mag.y * m_q.y + _2q1 * mag.z * m_q.z - mag.x * q2q2 - mag.x * q3q3;
+					hy = _2q0mx * m_q.z + mag.y * q0q0 - _2q0mz * m_q.x + _2q1mx * m_q.y - mag.y * q1q1 + mag.y * q2q2 + _2q2 * mag.z * m_q.z - mag.y * q3q3;
 					_2bx = sqrt(hx * hx + hy * hy);
-					_2bz = -_2q0mx * m_q.y + _2q0my * m_q.x + mz * q0q0 + _2q1mx * m_q.z - mz * q1q1 + _2q2 * my * m_q.z - mz * q2q2 + mz * q3q3;
+					_2bz = -_2q0mx * m_q.y + _2q0my * m_q.x + mag.z * q0q0 + _2q1mx * m_q.z - mag.z * q1q1 + _2q2 * mag.y * m_q.z - mag.z * q2q2 + mag.z * q3q3;
 					_4bx = 2.0f * _2bx;
 					_4bz = 2.0f * _2bz;
 
 					// Gradient decent algorithm corrective step
-					s0 = -_2q2 * (2.0f * q1q3 - _2q0q2 - ax) + _2q1 * (2.0f * q0q1 + _2q2q3 - ay) - _2bz * m_q.y * (_2bx * (0.5f - q2q2 - q3q3) + _2bz * (q1q3 - q0q2) - mx) + (-_2bx * m_q.z + _2bz * m_q.x) * (_2bx * (q1q2 - q0q3) + _2bz * (q0q1 + q2q3) - my) + _2bx * m_q.y * (_2bx * (q0q2 + q1q3) + _2bz * (0.5f - q1q1 - q2q2) - mz);
-					s1 = _2q3 * (2.0f * q1q3 - _2q0q2 - ax) + _2q0 * (2.0f * q0q1 + _2q2q3 - ay) - 4.0f * m_q.x * (1 - 2.0f * q1q1 - 2.0f * q2q2 - az) + _2bz * m_q.z * (_2bx * (0.5f - q2q2 - q3q3) + _2bz * (q1q3 - q0q2) - mx) + (_2bx * m_q.y + _2bz * m_q.w) * (_2bx * (q1q2 - q0q3) + _2bz * (q0q1 + q2q3) - my) + (_2bx * m_q.z - _4bz * m_q.x) * (_2bx * (q0q2 + q1q3) + _2bz * (0.5f - q1q1 - q2q2) - mz);
-					s2 = -_2q0 * (2.0f * q1q3 - _2q0q2 - ax) + _2q3 * (2.0f * q0q1 + _2q2q3 - ay) - 4.0f * m_q.y * (1 - 2.0f * q1q1 - 2.0f * q2q2 - az) + (-_4bx * m_q.y - _2bz * m_q.w) * (_2bx * (0.5f - q2q2 - q3q3) + _2bz * (q1q3 - q0q2) - mx) + (_2bx * m_q.x + _2bz * m_q.z) * (_2bx * (q1q2 - q0q3) + _2bz * (q0q1 + q2q3) - my) + (_2bx * m_q.w - _4bz * m_q.y) * (_2bx * (q0q2 + q1q3) + _2bz * (0.5f - q1q1 - q2q2) - mz);
-					s3 = _2q1 * (2.0f * q1q3 - _2q0q2 - ax) + _2q2 * (2.0f * q0q1 + _2q2q3 - ay) + (-_4bx * m_q.z + _2bz * m_q.x) * (_2bx * (0.5f - q2q2 - q3q3) + _2bz * (q1q3 - q0q2) - mx) + (-_2bx * m_q.w + _2bz * m_q.y) * (_2bx * (q1q2 - q0q3) + _2bz * (q0q1 + q2q3) - my) + _2bx * m_q.x * (_2bx * (q0q2 + q1q3) + _2bz * (0.5f - q1q1 - q2q2) - mz);
+					s0 = -_2q2 * (2.0f * q1q3 - _2q0q2 - acc.x) + _2q1 * (2.0f * q0q1 + _2q2q3 - acc.y) - _2bz * m_q.y * (_2bx * (0.5f - q2q2 - q3q3) + _2bz * (q1q3 - q0q2) - mag.x) + (-_2bx * m_q.z + _2bz * m_q.x) * (_2bx * (q1q2 - q0q3) + _2bz * (q0q1 + q2q3) - mag.y) + _2bx * m_q.y * (_2bx * (q0q2 + q1q3) + _2bz * (0.5f - q1q1 - q2q2) - mag.z);
+					s1 = _2q3 * (2.0f * q1q3 - _2q0q2 - acc.x) + _2q0 * (2.0f * q0q1 + _2q2q3 - acc.y) - 4.0f * m_q.x * (1 - 2.0f * q1q1 - 2.0f * q2q2 - acc.z) + _2bz * m_q.z * (_2bx * (0.5f - q2q2 - q3q3) + _2bz * (q1q3 - q0q2) - mag.x) + (_2bx * m_q.y + _2bz * m_q.w) * (_2bx * (q1q2 - q0q3) + _2bz * (q0q1 + q2q3) - mag.y) + (_2bx * m_q.z - _4bz * m_q.x) * (_2bx * (q0q2 + q1q3) + _2bz * (0.5f - q1q1 - q2q2) - mag.z);
+					s2 = -_2q0 * (2.0f * q1q3 - _2q0q2 - acc.x) + _2q3 * (2.0f * q0q1 + _2q2q3 - acc.y) - 4.0f * m_q.y * (1 - 2.0f * q1q1 - 2.0f * q2q2 - acc.z) + (-_4bx * m_q.y - _2bz * m_q.w) * (_2bx * (0.5f - q2q2 - q3q3) + _2bz * (q1q3 - q0q2) - mag.x) + (_2bx * m_q.x + _2bz * m_q.z) * (_2bx * (q1q2 - q0q3) + _2bz * (q0q1 + q2q3) - mag.y) + (_2bx * m_q.w - _4bz * m_q.y) * (_2bx * (q0q2 + q1q3) + _2bz * (0.5f - q1q1 - q2q2) - mag.z);
+					s3 = _2q1 * (2.0f * q1q3 - _2q0q2 - acc.x) + _2q2 * (2.0f * q0q1 + _2q2q3 - acc.y) + (-_4bx * m_q.z + _2bz * m_q.x) * (_2bx * (0.5f - q2q2 - q3q3) + _2bz * (q1q3 - q0q2) - mag.x) + (-_2bx * m_q.w + _2bz * m_q.y) * (_2bx * (q1q2 - q0q3) + _2bz * (q0q1 + q2q3) - mag.y) + _2bx * m_q.x * (_2bx * (q0q2 + q1q3) + _2bz * (0.5f - q1q1 - q2q2) - mag.z);
 					recipNorm = invSqrt(s0 * s0 + s1 * s1 + s2 * s2 + s3 * s3); // normalise step magnitude
 					s0 *= recipNorm;
 					s1 *= recipNorm;
@@ -141,7 +144,7 @@ namespace sbrcontroller {
 			//---------------------------------------------------------------------------------------------------
 			// IMU algorithm update
 
-			void UpdateIMU(Axis3DSensorData gyroData, Axis3DSensorData accelData)
+			void Madgwick::UpdateIMU(const Axis3DSensorData& gyroData, const Axis3DSensorData& accelData)
 			{
 				float recipNorm;
 				float s0, s1, s2, s3;
@@ -149,19 +152,20 @@ namespace sbrcontroller {
 				float _2q0, _2q1, _2q2, _2q3, _4q0, _4q1, _4q2 ,_8q1, _8q2, q0q0, q1q1, q2q2, q3q3;
 
 				// Rate of change of quaternion from gyroscope
-				qDot1 = 0.5f * (-m_q.x * gx - m_q.y * gy - m_q.z * gz);
-				qDot2 = 0.5f * (m_q.w * gx + m_q.y * gz - m_q.z * gy);
-				qDot3 = 0.5f * (m_q.w * gy - m_q.x * gz + m_q.z * gx);
-				qDot4 = 0.5f * (m_q.w * gz + m_q.x * gy - m_q.y * gx);
+				qDot1 = 0.5f * (-m_q.x * gyroData.x - m_q.y * gyroData.y - m_q.z * gyroData.z);
+				qDot2 = 0.5f * (m_q.w * gyroData.x + m_q.y * gyroData.z - m_q.z * gyroData.y);
+				qDot3 = 0.5f * (m_q.w * gyroData.y - m_q.x * gyroData.z + m_q.z * gyroData.x);
+				qDot4 = 0.5f * (m_q.w * gyroData.z + m_q.x * gyroData.y - m_q.y * gyroData.x);
 
 				// Compute feedback only if accelerometer measurement valid (avoids NaN in accelerometer normalisation)
-				if(!((ax == 0.0f) && (ay == 0.0f) && (az == 0.0f))) {
+				if(!((accelData.x == 0.0f) && (accelData.y == 0.0f) && (accelData.z == 0.0f))) {
 
 					// Normalise accelerometer measurement
-					recipNorm = invSqrt(ax * ax + ay * ay + az * az);
-					ax *= recipNorm;
-					ay *= recipNorm;
-					az *= recipNorm;   
+					Axis3DSensorData acc = accelData;
+					recipNorm = invSqrt(acc.x * acc.x + acc.y * acc.y + acc.z * acc.z);
+					acc.x *= recipNorm;
+					acc.y *= recipNorm;
+					acc.z *= recipNorm;   
 
 					// Auxiliary variables to avoid repeated arithmetic
 					_2q0 = 2.0f * m_q.w;
@@ -179,10 +183,10 @@ namespace sbrcontroller {
 					q3q3 = m_q.z * m_q.z;
 
 					// Gradient decent algorithm corrective step
-					s0 = _4q0 * q2q2 + _2q2 * ax + _4q0 * q1q1 - _2q1 * ay;
-					s1 = _4q1 * q3q3 - _2q3 * ax + 4.0f * q0q0 * m_q.x - _2q0 * ay - _4q1 + _8q1 * q1q1 + _8q1 * q2q2 + _4q1 * az;
-					s2 = 4.0f * q0q0 * m_q.y + _2q0 * ax + _4q2 * q3q3 - _2q3 * ay - _4q2 + _8q2 * q1q1 + _8q2 * q2q2 + _4q2 * az;
-					s3 = 4.0f * q1q1 * m_q.z - _2q1 * ax + 4.0f * q2q2 * m_q.z - _2q2 * ay;
+					s0 = _4q0 * q2q2 + _2q2 * acc.x + _4q0 * q1q1 - _2q1 * acc.y;
+					s1 = _4q1 * q3q3 - _2q3 * acc.x + 4.0f * q0q0 * m_q.x - _2q0 * acc.y - _4q1 + _8q1 * q1q1 + _8q1 * q2q2 + _4q1 * acc.z;
+					s2 = 4.0f * q0q0 * m_q.y + _2q0 * acc.x + _4q2 * q3q3 - _2q3 * acc.y - _4q2 + _8q2 * q1q1 + _8q2 * q2q2 + _4q2 * acc.z;
+					s3 = 4.0f * q1q1 * m_q.z - _2q1 * acc.x + 4.0f * q2q2 * m_q.z - _2q2 * acc.y;
 					recipNorm = invSqrt(s0 * s0 + s1 * s1 + s2 * s2 + s3 * s3); // normalise step magnitude
 					s0 *= recipNorm;
 					s1 *= recipNorm;
@@ -210,10 +214,10 @@ namespace sbrcontroller {
 				m_q.z *= recipNorm;
 
 				if (m_bSignalRequestReading) {
-					m_qOut.q0 = m_q.w;
-					m_qOut.q1 = m_q.x;
-					m_qOut.q2 = m_q.y;
-					m_qOut.q3 = m_q.z;
+					m_qOut.w = m_q.w;
+					m_qOut.x = m_q.x;
+					m_qOut.y = m_q.y;
+					m_qOut.z = m_q.z;
 					m_readingPromise.set_value(m_qOut);
 					m_bSignalRequestReading = false;
 				}
